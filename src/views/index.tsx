7f27eb85801c59f5a6d84fddb0ee52f0b35c080d
david-lake <lakedavid23@gmail.com>
2022-09-22 13:09:29 +0100

    Add not connected and connected containers

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -7,31 +7,92 @@ import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
 import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
 
+const metadata = {
+    "model": "metadata",
+    "address": "7vPWtXZkZiEYm4NKpMgW7eJUZ1TRcqjxg79jb6W1tS2h",
+    "mintAddress": "ApusBjwNZkoYtbzpgjNYRgkkQYG4Rx9iGiAD12Bf37Tt",
+    "updateAuthorityAddress": "9AhZV9edy7LvLbAMnXWxuKv1j5sch6BzhbSXvHA84kcX",
+    "json": null,
+    "jsonLoaded": false,
+    "name": "Skellers World #1349",
+    "symbol": "GRRR",
+    "uri": "https://arweave.net/kCJs_Bh6umaQDD5UCk19WX8Vw75wnUClip7aOephNHY",
+    "isMutable": true,
+    "primarySaleHappened": true,
+    "sellerFeeBasisPoints": 1000,
+    "editionNonce": 255,
+    "creators": [
+        {
+            "address": "3Z7f2YopXD4EaKfvM758x3HAmHBDJEx337XvzC2RHWkL",
+            "verified": true,
+            "share": 0
+        },
+        {
+            "address": "Abqf1Le5PAXjai5PM5TiPEaeoLviy1Br6TAAf3EBDsLr",
+            "verified": false,
+            "share": 25
+        },
+        {
+            "address": "7rMKpF2FSFQysJ2BfLJnsBXqmbEoqG2FR5qDwBZVZaBH",
+            "verified": false,
+            "share": 25
+        },
+        {
+            "address": "JBHoivwwRKhpTNatpf4f3wLpyYHgnFdEwHGWn2EpdeFq",
+            "verified": false,
+            "share": 50
+        }
+    ],
+    "tokenStandard": null,
+    "collection": null,
+    "collectionDetails": null,
+    "uses": null
+}
+
 export const HomeView: FC = ({ }) => {
   const wallet = useWallet();
   const connection = new Connection("https://devnet.genesysgo.net/", "confirmed");
-  const [allNFTs, setAllNFTs] = useState("");
+  const [nft, setNft] = useState();
 
   const metaplex = new Metaplex(connection)
   metaplex.use(walletAdapterIdentity(wallet));
 
   const getAllNFTs = async () => {
     const task = await metaplex.nfts().findAllByOwner(wallet.publicKey);
-    task.onStatusChange((status: TaskStatus) => console.log(status));
+    task.onSuccess(() => console.log(task.getResult()));
     task.run();
   }
 
+  const convertToJson = async () => {
+    fetch(metadata.uri)
+    .then((response) => response.json())
+    .then((json) => setNft(json))
+  }
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <p>Not connected</p>
+    </div>
+  )
+
+  const renderConnectedContainer = () => (
+    <div>
+      <p>Connected</p>
+    </div>
+  )
+
   useEffect(() => {
     if (wallet.publicKey) {
-      getAllNFTs()
+      convertToJson()
     }
-  }, [wallet.publicKey, getAllNFTs])
+  }, [wallet.publicKey, convertToJson])
 
   return (
 
     <div className="md:hero mx-auto p-4">
       <div className="text-center">
-        {wallet.publicKey && <p>Connected</p>}
+        {!wallet.publicKey && renderNotConnectedContainer()}
+        {wallet.publicKey && renderConnectedContainer()}
       </div>
     </div>
   );
